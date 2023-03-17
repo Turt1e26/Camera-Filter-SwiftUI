e7a4dd175f936298a11c716046faf1aa2d4862e3
Turt1e26 <bodiewolf26@gmail.com>
2023-03-17 09:44:45 -0700

    Added Files

---
+++ SwiftUI Camera/SwiftUI_CameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_CameraApp.swift
+//  SwiftUI Camera
+//
+//  Created by Bodie Woods on 3/16/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_CameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
